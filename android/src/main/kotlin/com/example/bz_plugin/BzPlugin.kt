9cec295f843882425ea86bb09459c7977dd90a71
David Nguyen <david.nguyen@agilelab.sg>
2022-06-28 16:00:52 +0700

    add ios lib to test

--- android/src/main/kotlin/com/example/bz_plugin/BzPlugin.kt
+++ android/src/main/kotlin/com/example/bz_plugin/BzPlugin.kt
@@ -1,11 +1,16 @@
 package com.example.bz_plugin
 
+import android.app.Activity
+import android.content.Context
 import android.os.Build
 import androidx.annotation.NonNull
-import com.cesarferreira.tempo.Tempo
+import com.bazaarvoice.bvandroidsdk.BVEventValues
+import com.bazaarvoice.bvandroidsdk.BVPageViewEvent
+import com.bazaarvoice.bvandroidsdk.BVPixel
 import com.cesarferreira.tempo.isToday
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,23 +18,28 @@ import io.flutter.plugin.common.MethodChannel.Result
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 /** BzPlugin */
-class BzPlugin: FlutterPlugin, MethodCallHandler {
+class BzPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context: Context
+  private lateinit var activity: Activity
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "bz_plugin")
     channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     when (call.method) {
       "getPlatformVersion" -> result.success("Android ${Build.VERSION.RELEASE}")
       "isToday" -> isToday(call, result)
+      "BVPageViewEvent" -> BVPageViewEvent(call, result)
       else -> {
         result.notImplemented()
       }
@@ -44,6 +54,26 @@ class BzPlugin: FlutterPlugin, MethodCallHandler {
     result.success(checkToday)
   }
 
+  private fun BVPageViewEvent(@NonNull call: MethodCall, @NonNull result: Result) {
+    var arguments = call.arguments as Map<String, Any>
+    var productId = arguments["productId"] as String
+    var category = arguments["category"] as String
+    var clientId = arguments["clientId"] as String
+
+    BVPixel.builder(context, clientId, false, false, Locale.getDefault())
+            .bgHandlerThread(optionalBackgroundHandlerThread)
+            .okHttpClient(optionalOkHttpClient)
+            .build();
+
+    val event = BVPageViewEvent(
+            productId,
+            BVEventValues.BVProductType.CONVERSATIONS_REVIEWS,
+            category
+    )
+
+    BVPixel.getInstance().track(event)
+  }
+
   private fun String.toDate(dateFormat: String = "yyyy-MM-dd'T'HH:mm:ss", timeZone: TimeZone = TimeZone.getTimeZone("UTC")): Date {
     val parser = SimpleDateFormat(dateFormat, Locale.getDefault())
     parser.timeZone = timeZone
@@ -52,5 +82,21 @@ class BzPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity;
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    TODO("Not yet implemented")
+  }
+
+  override fun onDetachedFromActivity() {
+    TODO("Not yet implemented")
   }
 }
--- ios/Classes/SwiftBzPlugin.swift
+++ ios/Classes/SwiftBzPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import SwiftDate
 
 public class SwiftBzPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,23 @@ public class SwiftBzPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+     switch call.method {
+    case "getPlatformVersion":
+        result("iOS " + UIDevice.current.systemVersion)
+    case "isToday":
+        isToday(call, result)
+    default:
+        result(nil)
+    }
   }
+  
+   private func isToday(_ call: FlutterMethodCall,_ result: @escaping FlutterResult) {
+        let arguments = call.arguments as! Dictionary<String, Any>
+        let dateTime = arguments["dateTime"] as! String;
+        // Convert to local
+        let localDate = dateTime.toDate(nil, region: Region.current)
+        // Check isToday
+        let checkToday = localDate?.isToday
+        result(checkToday)
+    }
 }
