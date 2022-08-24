d71944a0745fc7418aecba08ea506b45ffd59443
David Nguyen <david.nguyen@agilelab.sg>
2022-08-24 14:12:19 +0700

    remove import kotlinx.android.synthetic.*

--- android/src/main/kotlin/com/example/bz_plugin/BzPlugin.kt
+++ android/src/main/kotlin/com/example/bz_plugin/BzPlugin.kt
@@ -15,7 +15,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import kotlinx.android.synthetic.*
 import java.util.*
 
 
